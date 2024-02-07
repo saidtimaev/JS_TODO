53a2a6c303472bd9298f411092406b5399cfb9e0
saidtimaev <said.timaev@hotmail.com>
2024-02-07 16:26:42 +0100

    ajouté compteur Cards

--- script.js
+++ script.js
@@ -6,30 +6,44 @@ btn.onclick = ajouterCard
 const delBtn = card.querySelector(".delBtn")
 delBtn.addEventListener('click', supprimerCard)
 
+const affichageCompteur = document.querySelector(".compteurCards")
+
+// affichageCompteur.innerHTML = chiffreCompteur
+affichageCompteur.innerHTML = cards.children.length
+
 
 function ajouterCard() {
+
     const newCard = card.cloneNode(true); // Copie la 1ere card
     cards.appendChild(newCard); // Ajouté la card copiée au conteneur des cards
 
     const delBtn = newCard.querySelector(".delBtn")
     delBtn.addEventListener('click', supprimerCard)
 
+    afficherNbCards();
 }
 
 function supprimerCard(event) {
     // console.log("event = ", event);
 
-    // const cardSuprime = event.target;
+    
     // console.log("event.target = ", event.target);
 
-    // const todoCard = cardSuprime.closest('.todoCard');
+    
 
-    // cards.removeChild(todoCard);
+    cards.removeChild(event.target.closest('.todoCard'));
+
+    afficherNbCards();
 
     // console.log("event.target = ", event.target);
     // console.log("event.target.parentNode = ", event.target.parentNode);
     // console.log("event.target.parentNode.parentElement = ", event.target.parentNode.parentElement);
+    // event.target.parentNode.parentElement.remove();
+}
 
-    event.target.parentNode.parentElement.remove();
+function afficherNbCards(){
+
+    affichageCompteur.innerHTML = cards.children.length
+
 }
 
